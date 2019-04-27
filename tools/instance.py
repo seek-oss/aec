b1891e45a6c9966f373beccf9aaeccb3359faaab
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2019-04-27 16:37:28 +1000

    instance launching now works, and has a waiter

--- tools/instance.py
+++ tools/instance.py
@@ -1,40 +1,50 @@
+#!/usr/bin/env python
+
 import fire
 import boto3
 
+configs = {
+    "ap-southeast-2":
+        {
+            "amis": {"amazon_linux_2": "ami-04481c741a0311bbb",
+                    "ubuntu": "ami-0b76c3b150c6b1423"
+                    # TODO: automatically determine latest image
+                    },
 
-config = {
-    "ami": {"amazon_linux_2": "ami-04481c741a0311bbb",
-            "ubuntu": "ami-0b76c3b150c6b1423"
-            # TODO: automatically determine latest image
-            },
+            "key_name": "my-key",
 
-    "key_name": {"ap-southeast-2": "my-key"},
+            "security_group": "sg-12345678901234567",
 
-    "security_group": {"ap-southeast-2": "sg-12345678901234567"},
+            # TODO: multiple subnets
+            "subnet": "subnet-12345678",
 
-    # TODO: multiple subnets
-    "subnet": {"ap-southeast-2": "subnet-12345678"}
+
+            "iam_instance_profile_arn": "arn:aws:iam::123456789012:instance-profile/ec2_my_default"
+        }
 }
 
 
 def launch(name, owner, volume_size=100, ami_name="amazon_linux_2", instance_type="t2.medium", region="ap-southeast-2",
-           config=config):
+           configs=configs):
     ec2_client = boto3.client('ec2', region_name=region)
 
+    config = configs[region]
+
     response = ec2_client.run_instances(
-        ImageId=config['ami'][ami_name],
+        ImageId=config['amis'][ami_name],
         MaxCount=1, MinCount=1,
-        KeyName=config['key_name'][region],
+        KeyName=config['key_name'],
         InstanceType=instance_type,
-        TagSpecification=[
-            {'ResourceType': 'instance', 'Tag': [{'Key': 'Name', 'Value': name}, {'Key': 'Owner', 'Value': owner}]},
-            {'ResourceType': 'volume', 'Tag': [{'Key': 'Name', 'Value': name}, {'Key': 'Owner', 'Value': owner}]}
+        IamInstanceProfile={"Arn": config['iam_instance_profile_arn']},
+        TagSpecifications=[
+            {'ResourceType': 'instance', 'Tags': [{'Key': 'Name', 'Value': name}, {'Key': 'Owner', 'Value': owner}]},
+            {'ResourceType': 'volume', 'Tags': [{'Key': 'Name', 'Value': name}, {'Key': 'Owner', 'Value': owner}]}
         ],
-        EbsOptimized=True,
+        EbsOptimized=False if instance_type.startswith("t2") else True,
         NetworkInterfaces=[
             {'DeviceIndex': 0, 'Description': 'Primary network interface', 'DeleteOnTermination': True,
-             'SubnetId': config['subnet'][region], 'Ipv6AddressCount': 0,
-             'GroupSet': [config['security_group'][region]]}
+             'SubnetId': config['subnet'], 'Ipv6AddressCount': 0,
+             'Groups': [config['security_group']]}
         ],
         BlockDeviceMappings=[
             {'DeviceName': '/dev/xvda',
@@ -42,18 +52,17 @@ def launch(name, owner, volume_size=100, ami_name="amazon_linux_2", instance_typ
         ]
     )
 
-    instance_id = response['Instances'][0]['InstanceId']
+    instance = response['Instances'][0]
 
-    return response
+    waiter = ec2_client.get_waiter('instance_running')
+    waiter.wait(InstanceIds=[instance['InstanceId']])
 
-
-def hello():
-    print("hello")
-
-
-def world():
-    print("world")
+    return {'InstanceType': instance['InstanceType'],
+            'PrivateDnsName': instance['PrivateDnsName'],
+            'LaunchTime': instance['LaunchTime'],
+            'ImageId': instance['ImageId'],
+            'InstanceId': instance['InstanceId']}
 
 
 if __name__ == '__main__':
-    fire.Fire([launch, hello, world])
+    fire.Fire()
