c494b33a4814875370def593d2d681baeb6dcae5
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2019-04-27 17:32:16 +1000

    test now working

--- tests/test_instance.py
+++ tests/test_instance.py
@@ -7,44 +7,20 @@ from moto.ec2 import ec2_backends
 from tools.instance import launch
 
 
-@mock_ec2
 @pytest.fixture(scope="module")
 def mock_aws_configs():
-    region = 'ap-southeast-2'
     mock = mock_ec2()
     mock.start()
-
-    ec2 = boto3.resource('ec2', region)
-
-    # create VPC
-    vpc = ec2.create_vpc(CidrBlock='192.168.0.0/16')
-    vpc.wait_until_available()
-    print(vpc.id)
-
-    # create subnet
-    subnet = ec2.create_subnet(CidrBlock='192.168.1.0/24', VpcId=vpc.id)
-    # set subnet state to available
-    subnet.reload()
-    print(subnet.id)
-
-    # Create sec group
-    sec_group = ec2.create_security_group(GroupName='sg_0', Description='sg_0', VpcId=vpc.id)
-    print(sec_group.id)
-
-    print(AMIS[0]["ami_id"])
-    mock.stop()
-
-    print(ec2_backends[region].subnets)
+    region = 'ap-southeast-2'
 
     configs = {}
     configs[region] = {"amis": {"gami": AMIS[0]["ami_id"]},
                        "key_name": "test_key",
-                       "security_group": sec_group.id,
-                       "subnet": subnet.id,
+                       "security_group": "default",
+                       "subnet": next(ec2_backends[region].get_all_subnets()).id,
                        "iam_instance_profile_arn": "test_profile"}
     return configs
 
 
-@mock_ec2
 def test_launch(mock_aws_configs):
     launch("test", "alice@testlab.io", configs=mock_aws_configs)
