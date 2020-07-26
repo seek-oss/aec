9816b856a8e28b59abc9755b5b66a641bce51aad
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-07-26 21:28:32 +1000

    sort command help by function name

--- tools/main.py
+++ tools/main.py
@@ -4,10 +4,14 @@ import tools.ec2
 import tools.sqs
 
 
+def sort(functions):
+    return sorted(functions, key=lambda f: f.__name__)
+
+
 def main():
     parser = argh.ArghParser()
-    parser.add_commands(tools.ec2.cli.commands, namespace=tools.ec2.cli.namespace)
-    parser.add_commands(tools.sqs.cli.commands, namespace=tools.sqs.cli.namespace)
+    parser.add_commands(sort(tools.ec2.cli.commands), namespace=tools.ec2.cli.namespace)
+    parser.add_commands(sort(tools.sqs.cli.commands), namespace=tools.sqs.cli.namespace)
     parser.dispatch()
 
 
