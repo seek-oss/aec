68dfa48d9b3f1f28ac699b3526978881f91a14d0
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-08-09 22:52:58 +1000

    refactor: raise exception instead of print and exit

--- tools/config.py
+++ tools/config.py
@@ -1,9 +1,19 @@
 import os.path
-import sys
 from argparse import Namespace
 from typing import Any, Callable, Dict, Optional
 
 import pytoml as toml
+
+
+def inject_config(config_file: str) -> Callable[[Namespace], None]:
+    """Replace the "config" arg value with a dict loaded from the config file."""
+
+    def inner(namespace: Namespace) -> None:
+        # replace the "config" arg value with a dict loaded from the config file
+        if "config" in namespace:
+            setattr(namespace, "config", load_config(config_file, namespace.config))
+
+    return inner
 
 
 # TODO add tests for this
@@ -12,7 +22,7 @@ def load_config(config_file: str, profile: Optional[str] = None):
     Load profile from the config file.
 
     :param config_file:
-    :param profile: If default, use the value of the default key in the config file
+    :param profile: If None, use the value of the default key in the config file
     :return:
     """
     config_filepath = os.path.expanduser(config_file)
@@ -21,10 +31,7 @@ def load_config(config_file: str, profile: Optional[str] = None):
     # set profile to the value of the default key
     if not profile:
         if "default_profile" not in config:
-            print(
-                f"No profile supplied, or default profile set in {config_filepath}", file=sys.stderr,
-            )
-            exit(1)
+            raise Exception(f"No profile supplied, or default profile set in {config_filepath}")
         profile = config["default_profile"]
 
     # make top level keys available in the profile
@@ -34,23 +41,12 @@ def load_config(config_file: str, profile: Optional[str] = None):
     try:
         return config[profile]
     except KeyError:
-        print(f"Missing profile {profile} in {config_filepath}", file=sys.stderr)
-        exit(1)
+        raise Exception(f"Missing profile {profile} in {config_filepath}")
 
 
 def load_user_config_file(config_filepath) -> Dict[str, Any]:
     if not os.path.isfile(config_filepath):
-        print(f"No config file {config_filepath}", file=sys.stderr)
-        exit(1)
+        raise Exception(f"No config file {config_filepath}")
 
     with open(config_filepath) as config_file:
         return toml.load(config_file)
-
-
-def inject_config(config_file: str) -> Callable[[Namespace], None]:
-    def inner(namespace: Namespace) -> None:
-        # replace the "config" arg value with a dict loaded from the config file
-        if "config" in namespace:
-            setattr(namespace, "config", load_config(config_file, namespace.config))
-
-    return inner
