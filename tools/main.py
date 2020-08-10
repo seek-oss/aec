75a63f65cea0bb26d850176242358fe5bda6d4a2
Huy Nguyen <hunguyen@seek.com.au>
2020-08-10 16:14:42 +1000

    Specify argument type

--- tools/main.py
+++ tools/main.py
@@ -21,56 +21,56 @@ configure_cli = [
 ec2_cli = [
     Cmd(ec2.delete_image, [
         config_arg,
-        Arg("ami", help="AMI id")
+        Arg("ami", type=str, help="AMI id")
     ]),
     Cmd(ec2.describe, [
         config_arg,
-        Arg("--name", help="filter to hosts with this Name tag")
+        Arg("--name", type=str, help="filter to hosts with this Name tag")
     ]),
     Cmd(ec2.describe_images, [
         config_arg,
-        Arg("--ami", help="filter to this AMI id")
+        Arg("--ami", type=str, help="filter to this AMI id")
     ]),
     Cmd(ec2.launch, [
         config_arg,
-        Arg("name", help="Name tag of instance"),
-        Arg("ami", help="AMI id"),
-        Arg("--dist", help="linux distribution", choices=ec2.root_devices.keys(), default="amazon"),
-        Arg("--volume-size", help="ebs volume size (GB)", default=100),
-        Arg("--encrypted", help="Whether the EBS volume is encrypted", default=True),
-        Arg("--instance-type", help="instance type", default="t2.medium"),
-        Arg("--key-name", help="key name"),
-        Arg("--userdata", help="path to user data file")   
+        Arg("name", type=str, help="Name tag of instance"),
+        Arg("ami", type=str, help="AMI id"),
+        Arg("--dist", type=str, help="linux distribution", choices=ec2.root_devices.keys(), default="amazon"),
+        Arg("--volume-size", type=int, help="ebs volume size (GB)", default=100),
+        Arg("--encrypted", type=bool, help="Whether the EBS volume is encrypted", default=True),
+        Arg("--instance-type", type=str, help="instance type", default="t2.medium"),
+        Arg("--key-name", type=str, help="key name"),
+        Arg("--userdata", type=str, help="path to user data file")
     ]),
     Cmd(ec2.modify, [
         config_arg,
-        Arg("name", help="Name tag of instance"),
-        Arg("type", help="type of instance")
+        Arg("name", type=str, help="Name tag of instance"),
+        Arg("type", type=str, help="type of instance")
     ]),
     Cmd(ec2.share_image, [
         config_arg,
-        Arg("ami", help="AMI id"),
-        Arg("account", help="account id"),
+        Arg("ami", type=str, help="AMI id"),
+        Arg("account", type=str, help="account id"),
     ]), 
     Cmd(ec2.start, [
         config_arg,
-        Arg("name", help="Name tag of instance")
+        Arg("name", type=str, help="Name tag of instance")
     ]),
     Cmd(ec2.stop, [
         config_arg,
-        Arg("name", help="Name tag of instance")
+        Arg("name", type=str, help="Name tag of instance")
     ]),
     Cmd(ec2.terminate, [
         config_arg,
-        Arg("name", help="Name tag of instance")
+        Arg("name", type=str, help="Name tag of instance")
     ])
 ]
 
 sqs_cli = [
     Cmd(sqs.drain, [
         config_arg,
-        Arg("file_name", help="file to write messages to"),
-        Arg("--keep", help="keep messages, don't delete them", default=False)
+        Arg("file_name", type=str, help="file to write messages to"),
+        Arg("--keep", type=str, help="keep messages, don't delete them", default=False)
     ])
 ]
 # fmt: on
