732e4996f2dabe65109ba7ab4694e03755f2a4a6
Huy Nguyen <hunguyen@seek.com.au>
2020-08-10 16:20:36 +1000

    Clean up descriptions

--- tools/main.py
+++ tools/main.py
@@ -25,32 +25,32 @@ ec2_cli = [
     ]),
     Cmd(ec2.describe, [
         config_arg,
-        Arg("--name", type=str, help="filter to hosts with this Name tag")
+        Arg("--name", type=str, help="Filter to hosts with this Name tag")
     ]),
     Cmd(ec2.describe_images, [
         config_arg,
-        Arg("--ami", type=str, help="filter to this AMI id")
+        Arg("--ami", type=str, help="Filter to this AMI id")
     ]),
     Cmd(ec2.launch, [
         config_arg,
         Arg("name", type=str, help="Name tag of instance"),
         Arg("ami", type=str, help="AMI id"),
-        Arg("--dist", type=str, help="linux distribution", choices=ec2.root_devices.keys(), default="amazon"),
-        Arg("--volume-size", type=int, help="ebs volume size (GB)", default=100),
+        Arg("--dist", type=str, help="Linux distribution", choices=ec2.root_devices.keys(), default="amazon"),
+        Arg("--volume-size", type=int, help="EBS volume size (GB)", default=100),
         Arg("--encrypted", type=bool, help="Whether the EBS volume is encrypted", default=True),
-        Arg("--instance-type", type=str, help="instance type", default="t2.medium"),
-        Arg("--key-name", type=str, help="key name"),
-        Arg("--userdata", type=str, help="path to user data file")
+        Arg("--instance-type", type=str, help="Instance type", default="t2.medium"),
+        Arg("--key-name", type=str, help="Key name"),
+        Arg("--userdata", type=str, help="Path to user data file")
     ]),
     Cmd(ec2.modify, [
         config_arg,
         Arg("name", type=str, help="Name tag of instance"),
-        Arg("type", type=str, help="type of instance")
+        Arg("type", type=str, help="Type of instance")
     ]),
     Cmd(ec2.share_image, [
         config_arg,
         Arg("ami", type=str, help="AMI id"),
-        Arg("account", type=str, help="account id"),
+        Arg("account", type=str, help="Account id"),
     ]), 
     Cmd(ec2.start, [
         config_arg,
@@ -69,8 +69,8 @@ ec2_cli = [
 sqs_cli = [
     Cmd(sqs.drain, [
         config_arg,
-        Arg("file_name", type=str, help="file to write messages to"),
-        Arg("--keep", type=str, help="keep messages, don't delete them", default=False)
+        Arg("file_name", type=str, help="File to write messages to"),
+        Arg("--keep", type=str, help="Keep messages, don't delete them", default=False)
     ])
 ]
 # fmt: on
@@ -80,9 +80,9 @@ def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="aws easy cli")
     subparsers = parser.add_subparsers(title="commands")
 
-    cli.add_command_group(subparsers, "configure", "configure subcommands", configure_cli)
-    cli.add_command_group(subparsers, "ec2", "ec2 subcommands", ec2_cli, config.inject_config("~/.aec/ec2.toml"))
-    cli.add_command_group(subparsers, "sqs", "sqs subcommands", sqs_cli, config.inject_config("~/.aec/sqs.toml"))
+    cli.add_command_group(subparsers, "configure", "Configure subcommands", configure_cli)
+    cli.add_command_group(subparsers, "ec2", "EC2 subcommands", ec2_cli, config.inject_config("~/.aec/ec2.toml"))
+    cli.add_command_group(subparsers, "sqs", "SQS subcommands", sqs_cli, config.inject_config("~/.aec/sqs.toml"))
 
     return parser
 
