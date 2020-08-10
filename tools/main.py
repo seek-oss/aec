cc2e20914f68c03277f9eb7981bf41bb819f62b2
Huy Nguyen <36141077+huynguyent@users.noreply.github.com>
2020-08-10 16:44:58 +1000

    Fix argument bugs (#166)
    * Fix missing argument bug
    
    * Specify argument type
    
    * Clean up descriptions

--- tools/main.py
+++ tools/main.py
@@ -21,55 +21,56 @@ configure_cli = [
 ec2_cli = [
     Cmd(ec2.delete_image, [
         config_arg,
-        Arg("ami", help="AMI id")
+        Arg("ami", type=str, help="AMI id")
     ]),
     Cmd(ec2.describe, [
         config_arg,
-        Arg("--name", help="filter to hosts with this Name tag")
+        Arg("--name", type=str, help="Filter to hosts with this Name tag")
     ]),
     Cmd(ec2.describe_images, [
         config_arg,
-        Arg("--ami", help="filter to this AMI id")
+        Arg("--ami", type=str, help="Filter to this AMI id")
     ]),
     Cmd(ec2.launch, [
         config_arg,
-        Arg("name", help="Name tag of instance"),
-        Arg("ami", help="AMI id"),
-        Arg("--dist", help="linux distribution", choices=ec2.root_devices.keys(), default="amazon"),
-        Arg("--volume-size", help="ebs volume size (GB)", default=100),
-        Arg("--instance-type", help="instance type", default="t2.medium"),
-        Arg("--key-name", help="key name"),
-        Arg("--userdata", help="path to user data file")   
+        Arg("name", type=str, help="Name tag of instance"),
+        Arg("ami", type=str, help="AMI id"),
+        Arg("--dist", type=str, help="Linux distribution", choices=ec2.root_devices.keys(), default="amazon"),
+        Arg("--volume-size", type=int, help="EBS volume size (GB)", default=100),
+        Arg("--encrypted", type=bool, help="Whether the EBS volume is encrypted", default=True),
+        Arg("--instance-type", type=str, help="Instance type", default="t2.medium"),
+        Arg("--key-name", type=str, help="Key name"),
+        Arg("--userdata", type=str, help="Path to user data file")
     ]),
     Cmd(ec2.modify, [
         config_arg,
-        Arg("name", help="Name tag of instance"),
-        Arg("type", help="type of instance")
+        Arg("name", type=str, help="Name tag of instance"),
+        Arg("type", type=str, help="Type of instance")
     ]),
     Cmd(ec2.share_image, [
         config_arg,
-        Arg("ami", help="AMI id"),
-        Arg("account", help="account id"),
+        Arg("ami", type=str, help="AMI id"),
+        Arg("account", type=str, help="Account id"),
     ]), 
     Cmd(ec2.start, [
         config_arg,
-        Arg("name", help="Name tag of instance")
+        Arg("name", type=str, help="Name tag of instance")
     ]),
     Cmd(ec2.stop, [
         config_arg,
-        Arg("name", help="Name tag of instance")
+        Arg("name", type=str, help="Name tag of instance")
     ]),
     Cmd(ec2.terminate, [
         config_arg,
-        Arg("name", help="Name tag of instance")
+        Arg("name", type=str, help="Name tag of instance")
     ])
 ]
 
 sqs_cli = [
     Cmd(sqs.drain, [
         config_arg,
-        Arg("file_name", help="file to write messages to"),
-        Arg("--keep", help="keep messages, don't delete them", default=False)
+        Arg("file_name", type=str, help="File to write messages to"),
+        Arg("--keep", type=str, help="Keep messages, don't delete them", default=False)
     ])
 ]
 # fmt: on
@@ -79,9 +80,9 @@ def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="aws easy cli")
     subparsers = parser.add_subparsers(title="commands")
 
-    cli.add_command_group(subparsers, "configure", "configure subcommands", configure_cli)
-    cli.add_command_group(subparsers, "ec2", "ec2 subcommands", ec2_cli, config.inject_config("~/.aec/ec2.toml"))
-    cli.add_command_group(subparsers, "sqs", "sqs subcommands", sqs_cli, config.inject_config("~/.aec/sqs.toml"))
+    cli.add_command_group(subparsers, "configure", "Configure subcommands", configure_cli)
+    cli.add_command_group(subparsers, "ec2", "EC2 subcommands", ec2_cli, config.inject_config("~/.aec/ec2.toml"))
+    cli.add_command_group(subparsers, "sqs", "SQS subcommands", sqs_cli, config.inject_config("~/.aec/sqs.toml"))
 
     return parser
 
