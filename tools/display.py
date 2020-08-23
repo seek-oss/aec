b47a87870363c9c122f3e415dbe6e9d2a0f8b55a
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-08-23 22:42:13 +1000

    use simple table format

--- tools/display.py
+++ tools/display.py
@@ -1,5 +1,6 @@
 import json
 from typing import Any, Dict, List, Optional, Sequence, cast
+from rich import box
 
 from rich.console import Console
 from rich.table import Table
@@ -64,7 +65,7 @@ def pretty_print(result):
             return
 
         column_names = cast(List[str], rows[0])
-        table = Table()
+        table = Table(box = box.SIMPLE)
         for c in column_names:
             table.add_column(c)
 
