4b4e21a805708af7bec7621af7015302c9624fb8
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-08-28 10:38:06 +1000

    bump 0.4.5 - fix ModuleNotFoundError

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = Path("README.md").read_text()
 
 setup(
     name="aec",
-    version="0.4.4",
+    version="0.4.5",
     description="AWS Easy CLI",
     long_description=long_description,
     long_description_content_type="text/markdown",
