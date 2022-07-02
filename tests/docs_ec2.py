87fff626cc5a901fa896ee86197b0095e63ef78c
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-07-02 17:55:40 +1000

    minimal example

--- tests/docs_ec2.py
+++ tests/docs_ec2.py
@@ -1,5 +1,4 @@
 from moto import mock_ec2
-from moto.ec2 import ec2_backends
 from moto.ec2.models.amis import AMIS
 
 from aec import main
@@ -7,30 +6,10 @@ from aec.command.ec2 import launch
 
 ami_id = AMIS[0]["ami_id"]
 
+mock = mock_ec2()
+mock.start()
 
-def mock_aws_config():
-    mock = mock_ec2()
-    mock.start()
-    region = "us-east-1"
+launch({"key_name": "test_key"}, "alice", ami_id)
+launch({"key_name": "test_key"}, "sam", ami_id)
 
-    return {
-        "region": region,
-        "key_name": "test_key",
-        "vpc": {
-            "name": "test vpc",
-            "subnet": next(ec2_backends[region].get_all_subnets()).id,
-            "security_group": "default",
-        },
-        "iam_instance_profile_arn": "test_profile",
-    }
-
-
-def describe(config):
-    launch(config, "alice", ami_id)
-    launch(config, "sam", ami_id)
-
-    main.main(["ec2", "status"])
-
-
-if __name__ == "__main__":
-    describe(mock_aws_config())
+main.main(["ec2", "status"])
