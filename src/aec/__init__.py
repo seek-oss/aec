seek-oss/aec

commit b6bf722df6f300055714731677a5809737ff5b2e

fix: correctly type describe_images_owners in config (#381)
The describe_images_owners key is either a string or a List of string. Logic for both cases is in https://github.com/seek-oss/aec/blob/master/src/aec/command/ami.py#L76-L83, and usage is illustrated by the example at https://github.com/seek-oss/aec/blob/master/src/aec/config-example/ec2.toml.

src/aec/util/config.py
@@ -1,6 +1,6 @@
 import os.path
 from argparse import Namespace
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Callable, Dict, Optional, List, Union
 
 import pytoml as toml
 from typing_extensions import TypedDict
@@ -26,7 +26,7 @@ class Config(TypedDict, total=False):
     additional_tags: Dict[str, str]
     iam_instance_profile_arn: str
     kms_key_id: str
-    describe_images_owners: str
+    describe_images_owners: Union[List[str], str]
     describe_images_name_match: str
 
 

commit 98416a70099630f3436439eb36d7e57b56481149

fix: lints and release 2.5.9 (#383)
fix linting errors introduced in #381 ... the CI build will now run on PR to catch these (see #382)

src/aec/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "2.5.8"
+__version__ = "2.5.9"

src/aec/util/config.py
@@ -1,6 +1,6 @@
 import os.path
 from argparse import Namespace
-from typing import Any, Callable, Dict, Optional, List, Union
+from typing import Any, Callable, Dict, List, Optional, Union
 
 import pytoml as toml
 from typing_extensions import TypedDict