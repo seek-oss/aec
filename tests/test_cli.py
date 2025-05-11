32f98599495cd8d254ba6551d67e037f29979393
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-05-11 10:15:16 +1000

    ci: add test_terminate_with_empty_string_fails
    for code introduced in #497

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 import argparse
 from typing import Any
 
+import pytest
+
 import aec.util.cli as cli
 import aec.util.config as config
+from aec.main import build_parser
 from aec.util.cli import Arg, Cmd
 
 
@@ -28,3 +31,13 @@ def test_cli_injects_config():
     cli.dispatch(parser, args=["food", "eat", "--config", "us", "cheese", "--temp", "warm"])
     # When --config isn't supplied, the default profile from the config file is passed in
     cli.dispatch(parser, args=["food", "eat", "cheese", "--temp", "warm"])
+
+
+def test_terminate_with_empty_string_fails(capsys: pytest.CaptureFixture):
+    parser = build_parser()
+
+    with pytest.raises(SystemExit):
+        parser.parse_args(["ec2", "terminate", ""])
+
+    _stdout, stderr = capsys.readouterr()
+    assert "is empty string" in stderr
